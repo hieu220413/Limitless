209081137a85a5012265feacad41a0366939ffb6
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-05-18 23:06:58 +0700

    base security + swagger (not run yet)

---
+++ LimitLessAPI/src/main/java/service/UserSecurityService.java
@@ -0,0 +1,31 @@
+package service;
+
+import entity.Users;
+import model.UserDetailsPrincipalModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+//    @Autowired
+//    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String test = new BCryptPasswordEncoder().encode("123admin");
+//        UserEntity userEntity = userRepository.findByUsername(username);
+        Users userEntity = null;
+//        UserDetailsPrincipalModel user = UserDetailsPrincipalModel.builder()
+//                .username(userEntity.getUsername())
+//                .password(userEntity.getPassword())
+//                .role(User.RoleEnum.valueOf(userEntity.getRole()))
+//                .build();
+        UserDetailsPrincipalModel user = null;
+        UserDetailsPrincipal userDetailsPrincipal = new UserDetailsPrincipal(user);
+        return userDetailsPrincipal;
+    }
+}
