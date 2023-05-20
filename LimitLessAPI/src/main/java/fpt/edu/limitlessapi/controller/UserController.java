bc1d5e0d1b278701c6487b7e335a61b3198662a0
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-05-20 22:53:57 +0700

    imple sign up

--- LimitLessAPI/src/main/java/fpt/edu/limitlessapi/controller/UserController.java
+++ LimitLessAPI/src/main/java/fpt/edu/limitlessapi/controller/UserController.java
@@ -1,19 +1,23 @@
 package fpt.edu.limitlessapi.controller;
 
 import fpt.edu.limitlessapi.exception.AuthFailException;
+import fpt.edu.limitlessapi.exception.InvalidUserInputException;
 import fpt.edu.limitlessapi.model.ApiError;
 import fpt.edu.limitlessapi.model.LoginBody;
 import fpt.edu.limitlessapi.model.UserResponseModel;
+import fpt.edu.limitlessapi.model.UserSignUpBody;
 import fpt.edu.limitlessapi.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(value = "user")
@@ -23,6 +27,11 @@ public class UserController {
     public ResponseEntity<ApiError> handlerAuthFailException(AuthFailException e) throws IOException {
         ApiError apiError = new ApiError(new Date(), e.getStatusCode().value(), e.getReason(), "wrong password or username");
         return ResponseEntity.status(HttpStatus.valueOf(e.getStatusCode().value())).body(apiError);
+    }
+    @ExceptionHandler(InvalidUserInputException.class)
+    public ResponseEntity<ApiError> handlerInvalidUserInputException(InvalidUserInputException e){
+        ApiError apiError = new ApiError(new Date(), e.getStatusCode().value(), e.getReason(), "Invalid input fields", e.getErrorFields());
+        return ResponseEntity.status(HttpStatusCode.valueOf(e.getStatusCode().value())).body(apiError);
     }
 
     @Autowired
@@ -34,5 +43,13 @@ public class UserController {
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth(loginBody.getUsername(), loginBody.getPassword());
         return ResponseEntity.status(HttpStatus.OK).headers(headers).body(userResponseModel);
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public ResponseEntity<HashMap> SignUp(@Valid @RequestBody UserSignUpBody userSignUpBody) {
+        userService.signUp(userSignUpBody);
+        HashMap<String,String> message = new HashMap<>();
+        message.put("message", "sign up successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 }
