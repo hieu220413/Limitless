7eb069a07cae4d48ca4b7be8a1ec0b0377a46951
hieu <nguyentrunghieu13012002@gmail.com>
2023-05-24 20:10:21 +0700

    update mainpage without footer

--- LimitLess/App.tsx
+++ LimitLess/App.tsx
@@ -7,13 +7,16 @@ import Old from './screen/Old';
 import Weight from './screen/Weight';
 import Height from './screen/Height';
 import Goals from './screen/Goals';
+import MainPage from './screen/MainPage';
+
 
 
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator  screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Main" component={MainPage} options={({ route, navigation }) => ({})} />
         <Stack.Screen name="Gender" component={Gender} options={({ route, navigation }) => ({})} />
         <Stack.Screen name="Old" component={Old} options={({ route, navigation }) => ({})} />
         <Stack.Screen name="Weight" component={Weight} options={({ route, navigation }) => ({})} />
