8e21b77d8b872ba991fd8ab0597e1a0d4c625992
giavinh <82934507+luugiavinh@users.noreply.github.com>
2023-05-27 13:48:50 +0700

    login integrate

--- LimitLess/screen/signIn_Up/Login.jsx
+++ LimitLess/screen/signIn_Up/Login.jsx
@@ -16,6 +16,8 @@ import { SignInUpLayout, SignInUpLayoutBody } from '../../component/Authen-layou
 import { useFocusEffect } from '@react-navigation/native';
 import { AvoidSoftInput } from 'react-native-avoid-softinput';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Login = ({ navigation }) => {
   // const onFocusEffect = () => {
@@ -29,30 +31,55 @@ const Login = ({ navigation }) => {
   // useFocusEffect(onFocusEffect)
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [usernameInput, setUsernameInput] = useState('')
+  const [passwordInput, setPasswordInput] = useState('')
+  const [errorLoginMessage, setErrorLoginMessage] = useState('')
+  const signIn = async () => {
+    const loginResponseBody = await fetch('http://10.0.2.2:8080/api/user/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: usernameInput,
+        password: passwordInput,
+      }),
+    }).then(response => response.json()).then(json => json).catch(error => console.log(error))
+
+    if (!loginResponseBody.error){
+      console.log(JSON.stringify(loginResponseBody))
+      await AsyncStorage.setItem('user_info', JSON.stringify(loginResponseBody))
+      navigation.reset({ index: 0, routes: [{ name: 'Gender' }] })
+    } else{
+      console.log(JSON.stringify(loginResponseBody))
+      setErrorLoginMessage(loginResponseBody.message)
+    }
+}
   return (
     <SignInUpLayout>
       <SignInUpLayoutBody>
         <Text style={styles.titleStyle}>Login to your Account</Text>
         <View style={styles.formFieldGroupStyle}>
           <View>
-            <TextInput style={styles.textInputStyle} placeholder='Username' />
-            <Text style={styles.errorInputStyle}>Invalid username</Text>
+            <TextInput style={styles.textInputStyle} placeholder='Username' onChangeText={newText => setUsernameInput(newText)} defaultValue={usernameInput} />
+            {errorLoginMessage.length != 0 && <Text style={styles.errorInputStyle}>{errorLoginMessage}</Text>}
           </View>
           <View>
-            <TextInput style={styles.textInputStyle} placeholder='Password' />
-            <Text style={styles.errorInputStyle}>Invalid password</Text>
+            <TextInput style={styles.textInputStyle} placeholder='Password' onChangeText={newText => setPasswordInput(newText)} defaultValue={passwordInput} />
+            {errorLoginMessage.length != 0 && <Text style={styles.errorInputStyle}>{errorLoginMessage}</Text>}
           </View>
           <View style={{ flexDirection: 'row', alignItems: 'center', columnGap: 4 }}>
             <Switch
-              style={{ alignSelf: 'flex-start', transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }]}}
+              style={{ alignSelf: 'flex-start', transform: [{ scaleX: 1.2 }, { scaleY: 1.2 }] }}
               trackColor={{ false: '#767577', true: '#7CD367' }}
               thumbColor={'#EEEEEE'}
               onValueChange={toggleSwitch}
               value={isEnabled}
             ></Switch>
-            <Text style={{fontSize: 18}}>Remember me</Text>
+            <Text style={{ fontSize: 18 }}>Remember me</Text>
           </View>
-          <TouchableHighlight style={styles.buttonResgisterStyle} underlayColor="#461CF0" onPress={() => { navigation.reset({ index: 0, routes: [{ name: 'Gender' }] }) }}>
+          <TouchableHighlight style={styles.buttonResgisterStyle} underlayColor="#461CF0" onPress={() => { signIn() }}>
             <View>
               <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', color: 'white' }}>Sign in</Text>
             </View>
@@ -60,7 +87,7 @@ const Login = ({ navigation }) => {
         </View>
         <View style={{ flexDirection: 'row', columnGap: 5, alignItems: 'center', marginBottom: 10 }}>
           <View style={[styles.Line, { flex: 1 }]} />
-          <Text style={{fontSize: 20, color: '#15186D' }}>Or sign up with</Text>
+          <Text style={{ fontSize: 20, color: '#15186D' }}>Or sign up with</Text>
           <View style={[styles.Line, { flex: 1 }]} />
         </View>
         <View style={styles.buttonLoginWithGroupStyle}>
@@ -76,8 +103,8 @@ const Login = ({ navigation }) => {
         </View>
         <View style={{ flexGrow: 1, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center' }}>
           <View style={{ flexDirection: 'row', columnGap: 5 }}>
-            <Text style={{ textAlign: 'center' , fontSize: 18}}>Don’t have an account?</Text>
-            <TouchableOpacity><Text style={{ fontWeight: 'bold', fontSize: 18}} onPress={() => { navigation.navigate('Register') }}>Signup</Text></TouchableOpacity>
+            <Text style={{ textAlign: 'center', fontSize: 18 }}>Don’t have an account?</Text>
+            <TouchableOpacity><Text style={{ fontWeight: 'bold', fontSize: 18 }} onPress={() => { navigation.navigate('Register') }}>Signup</Text></TouchableOpacity>
           </View>
         </View>
       </SignInUpLayoutBody>
@@ -118,7 +145,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#461CF0',
     marginBottom: 40,
     marginTop: 10
-  }, 
+  },
   textInputStyle: {
     backgroundColor: '#E6E6E6',
     borderRadius: 50,
