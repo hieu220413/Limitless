316f2dbbfb464418430401864228c4a795fda2c2
Quang <hongquang7302@gmail.com>
2023-06-01 19:49:51 +0700

    commit

---
+++ landing-page/src/components/Join/Join.jsx
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
+
+const Join = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_kbr2qks",
+        "template_h0j9l6f",
+        form.current,
+        "I898Rr5TeDuLs4iDZ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="Join" id="join">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your Email address"
+          ></input>
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
---
+++ landing-page/src/data/testimonialsData.js
@@ -0,0 +1,35 @@
+import image1 from "../assets/Hieu.jpeg";
+import image2 from "../assets/Khoa.jpeg";
+import image3 from "../assets/Hiep.jpeg";
+import image4 from "../assets/Quang.jpeg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Limitless and by choosing the right plan and program I already achieved my ideal body!",
+    name: "Hieu Nguyen",
+    status: "ENTREPRENEUR",
+  },
+  {
+    image: image4,
+    review:
+      "Limitless is a variety of exercises and easy to do, divided into three levels for beginner, advanced and advanced. The app also calculates calories burned and exercise time in the form of a visual graph.",
+    name: "Quang Pham",
+    status: "CEO",
+  },
+  {
+    image: image2,
+    review:
+      "This is an ideal place for us to train ourselves. Come and have an interesting experience for yourself.",
+    name: "Khoa Dang",
+    status: "COACH",
+  },
+  {
+    image: image3,
+    review:
+      "I am very satisfied with this app and it really gives me a great experience when training.",
+    name: "Hiep Dang",
+    status: "CUSTOMER",
+  },
+];
